ac61f118b6e5a42efa488a1c223821b02a20a1a7
Gourav Raj <gourav.raj42001@gmail.com>
2022-11-03 19:04:44 +0530

    Fully Functional

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
-const random = Math.floor((Math.random() * 100) + 1);
-let gueses = []
-// console.log(random)
+let random = Math.floor((Math.random()*100)+1)
+console.log(random);
+let history=[]
+
+
+function reset(){
+    random = Math.floor((Math.random()*100)+1)
+    console.log(random);
+    document.getElementById("result").innerHTML=''
+    history = [];
+    showHistory()
+}
+
+
 function play(){
 
-    let n = document.getElementById("numberInput").value;
-    displayResult(n)
-    displayHistory()
+    let n = document.getElementById("inputNumber").value
+    showResult(n)
+    showHistory()
+
 }
-function displayResult(n)
-{
-    if(random==n)
-    {
-        document.getElementById("result").innerHTML = 'Awesome, You Guessed Right'
-    }
+
+function showResult(n){
+
+    history.push(n)
+    if(n==random)
+        dialog = 'Awesome, You Guessed Right ....'
     else if(n>random)
-    {
-        document.getElementById("result").innerHTML = 'Number is too High'
-        gueses.push(n);
-
-
-    }else{
-        document.getElementById("result").innerHTML = 'Number is too Low'
-        gueses.push(n);
-    }
+        dialog = 'Too High'
+    else
+        dialog = 'Too Low'
+    document.getElementById("result").innerHTML = dialog
 }
-function displayHistory(){
-    let dialog=''
-    for(let i=gueses.length-1;i>=0;i--)
-        dialog += '<ul> You Guessed '+gueses[i]+'</ul>'
-    console.log(dialog)
-    document.getElementById("history").innerHTML = dialog;
+
+function showHistory(){
+
+    let dialog = ''
+    for(let i=history.length-1;i>=0;i--)
+        dialog+='<ul> You Guessed '+history[i]+'</ul>'
+    document.getElementById("history").innerHTML = dialog
     
-}
-function yo(){
-    console.log(gueses)
 }
